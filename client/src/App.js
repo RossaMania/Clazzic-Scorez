d3711ae68090dc4dd0363fb0c0200b406781336f
Ross Clettenberg <raclettenberg@gmail.com>
2022-11-25 17:04:27 -0600

    Create skeleton frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Games from './pages/Games';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/games" element={<Games />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Games.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Games() {
+  return (
+    <h1>I am games!</h1>
+  );
+};
+export default Games;
