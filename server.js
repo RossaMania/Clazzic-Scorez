c66ca6e7ac4cb595c087f680e410273d4bc4ccda
Ross Clettenberg <raclettenberg@gmail.com>
2022-11-24 18:05:18 -0600

    Add api route.

--- server.js
+++ server.js
@@ -21,7 +21,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(gameRoutes)
+app.use('/api',gameRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
+
 
 const gameEntry = new Game({
 
