7c6de3a0c5226de09b9c77a50acd5470b014cd75
Ross Clettenberg <raclettenberg@gmail.com>
2023-05-24 18:04:15 -0500

    Made score be a proper object. Add input for name. Add name property for inputs. Edit value properties for inputs.

--- client/src/pages/Scores.js
+++ client/src/pages/Scores.js
@@ -1,34 +1,42 @@
 import React, { useEffect, useState } from "react";
 
 function Scores() {
-  const [inputScore, setInputScore] = useState("");
+  const [inputScore, setInputScore] = useState({
+    name: "",
+    score:""
+  });
 
   const [scores, setScores] = useState([]);
 
   function handleChange(event) {
-    const newValue = event.target.value;
-    console.log(newValue);
-    setInputScore(newValue);
+    const { name, value } = event.target;
+    setInputScore((prevScore) => {
+      return {
+        ...prevScore,
+        [name]: value
+      }
+    })
   }
 
   function addScore(score) {
+    console.log(score);
     setScores((prevScores) => {
       return [...prevScores, score];
     });
     console.log("Score added! A winner is you!");
-    console.log(score);
   }
 
   return (
     <div>
       <form>
-        <input onChange={handleChange} type="text" value={inputScore} />
+        <input name="name" onChange={handleChange} type="text" value={inputScore.name} />
+        <input name="score" onChange={handleChange} type="text" value={inputScore.score} />
         <button onClick={addScore}>Submit Score!</button>
       </form>
       <div>
         <ul>
           {scores.map(score => {
-            return <li>{score.score}</li>
+            return <li>{score.name} {score.score}</li>
           })}
         </ul>
       </div>
